0280b24a331bc2ecfe8b1b1c98a3bae88e6ab1dc
Nic Wortel <nd.wortel@gmail.com>
2015-03-10 17:13:27 +0100

    Add unit test for Route class

---
+++ tests/Unit/Mapping/RouteTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RAPL\Tests\Unit\Mapping;
+
+use RAPL\RAPL\Mapping\Route;
+
+class RouteTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRoute()
+    {
+        $pattern   = 'foo/bar/{id}';
+        $envelopes = array('foo', 'bar');
+
+        $route = new Route($pattern, $envelopes);
+
+        $this->assertSame($pattern, $route->getPattern());
+        $this->assertSame($envelopes, $route->getEnvelopes());
+    }
+}
