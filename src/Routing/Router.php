85fb53e793bfd81581320efe7616e7f8e8290a3f
Nic Wortel <nd.wortel@gmail.com>
2015-03-11 10:57:19 +0100

    Rename internal method parameter

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -26,37 +26,37 @@ class Router implements RouterInterface
 
     /**
      * @param Route $route
-     * @param array $parameters
+     * @param array $conditions
      *
      * @return string
      */
-    protected function parseUrl(Route $route, array $parameters)
+    protected function parseUrl(Route $route, array $conditions)
     {
         $uri = $route->getPattern();
 
-        foreach ($parameters as $parameter => $value) {
+        foreach ($conditions as $parameter => $value) {
             $count = 0;
             $uri   = str_replace(sprintf('{%s}', $parameter), $value, $uri, $count);
 
             if ($count > 0) {
-                unset($parameters[$parameter]);
+                unset($conditions[$parameter]);
             }
         }
 
-        if (count($parameters) > 0) {
-            $uri .= '?' . $this->buildQueryString($parameters);
+        if (count($conditions) > 0) {
+            $uri .= '?' . $this->buildQueryString($conditions);
         }
 
         return $uri;
     }
 
     /**
-     * @param array $parameters
+     * @param array $conditions
      *
      * @return string
      */
-    protected function buildQueryString(array $parameters)
+    protected function buildQueryString(array $conditions)
     {
-        return http_build_query($parameters);
+        return http_build_query($conditions);
     }
 }
